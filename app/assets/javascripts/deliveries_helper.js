906c30c91301a17817f2e6e4707d2490426de731
FruityFree <kirill.fedyanin@phystech.edu>
2013-10-18 12:28:40 +0400

    fix the b-name-problem

---
+++ app/assets/javascripts/deliveries_helper.js
@@ -0,0 +1,12 @@
+//function to add the row. Content is json object with fields:
+//code(true is ok, false is not ok); company; typeName; price; days.
+//table is jquery object of target table
+var  add_row = function(content,table){
+ 	var row = "<tr>";
+ 	row += "<td><a href='"+content.companyLink+"'>"+content.companyName+"</a></td>";
+ 	row += "<td>"+content.typeName+"</td>";
+ 	row += "<td>"+content.price+"</td>";
+ 	row += "<td>"+content.days+"</td>";
+ 	row +="</tr>";
+ 	table.append(row);
+};
