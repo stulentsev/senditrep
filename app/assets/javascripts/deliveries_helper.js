12bed0bd951e9f48789a6103980419aa3e863657
FruityFree <kirill.fedyanin@phystech.edu>
2013-10-30 11:37:58 +0400

    alt for images

--- app/assets/javascripts/deliveries_helper.js
+++ app/assets/javascripts/deliveries_helper.js
@@ -4,7 +4,8 @@
 var  addRow = function(content,table){
  	var row = "<tr>";
  	row += "<td><a href = '"+content.companyLink;
- 	row +="'><img src = '" + content.image_url +"'/></a></td>";
+ 	row += "'><img src = '" + content.image_url;
+ 	row += "'alt ='"+ content.companyName +"'/></a></td>";
  	row += "<td>"+content.typeName+"</td>";
  	row += "<td>"+content.price+"</td>";
  	row += "<td>"+content.days+"</td>";
@@ -16,7 +17,4 @@ var prepareParameters = function(params){
 	var result = params.replace(new RegExp("&quot;", "g"),"'");
 	result = result.replace(new RegExp("=&gt;", "g"),":");
 	return(result);
-}
-
-
- 	
+}	
